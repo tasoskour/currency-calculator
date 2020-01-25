a1fab63d7a4d02395c0ffe06dc7b53a9461bacf5
Tasos Kourtis <tasoskour1@gmail.com>
2020-01-25 19:42:49 +0200

    minor msg fix

--- curr_calc/app/Http/Controllers/CurrenciesController.php
+++ curr_calc/app/Http/Controllers/CurrenciesController.php
@@ -32,46 +32,43 @@ public function getExRate( $b_c,$t_c){
 public function ExchangeRateCreate(Request $request){
 
 $validator = Validator::make($request->all(),[
-    'baseCur' => [
-        'required',
-        'max:25',
-        /*Create only unique combination for target and base currency*/
-        Rule::unique('rates')->where(function ($query) use($request) {
-            return $query->where('baseCur', $request->baseCur)
-            ->where('targetCur', $request->targetCur);
+      'baseCur' => [
+      'required',
+      'max:25',
+      /*Create only for unique combination of target and base currency*/
+      Rule::unique('rates')->where(function ($query) use($request) {
+              return $query->where('baseCur', $request->baseCur)
+              ->where('targetCur', $request->targetCur);
         }),
-    ],
-    'targetCur' =>'required||max:25',
-    'rate' => 'required|numeric|between:0,99999.999999',
-
-  ]);
+      ],
+     'targetCur' =>'required||max:25',
+     'rate' => 'required|numeric|between:0.000001,99999.999999',
+   ]);
 
    if($validator->fails()){
       $messages = $validator->messages();
             return response()->json($messages->all(),422);
-        }
-
-     else{$exrate = ExchangeRate::create($request->all());
+   }
+   else{$exrate = ExchangeRate::create($request->all());
          return response()->json(["Added successfully"], 201) ;
-       }
+   }
 }
 //Update existing exchange rate and currencies
 public function update(Request $request,ExchangeRate $exchangeId){
 
   $validator = Validator::make($request->all(),[
       'baseCur' => [
-          'required',
-          'max:25',
-          /*Create only unique combination for target and base currency*/
-          Rule::unique('rates')->where(function ($query) use($request,$exchangeId) {
+      'required',
+      'max:25',
+       /*Update only with unique names for the currencies*/
+       Rule::unique('rates')->where(function ($query) use($request,$exchangeId) {
               return $query->where('baseCur', $request->baseCur)
               ->where('targetCur', $request->targetCur)
               ->where('id','!=', $exchangeId->id);
-
           }),
       ],
       'targetCur' =>'required||max:25',
-      'rate' => 'required|numeric|between:0,99999.999999',
+      'rate' => 'required|numeric|between:0.000001,99999.999999',
 
     ]);
 
@@ -81,14 +78,14 @@ public function update(Request $request,ExchangeRate $exchangeId){
        }
    else{  $exchangeId->update($request->all());
           $updateMesg=array("Updated successfully");
-          return response()->json($updateMesg,200);}
-
+          return response()->json($updateMesg,200);
+   }
 }
 
 //Delete exchange rate by id
 public function delete(ExchangeRate $exchangeId){
   $exchangeId->delete();
-  $deleteMsg=array("Delete successfully");
+  $deleteMsg=array("Deleted successfully");
    return $deleteMsg;
 }
 }
